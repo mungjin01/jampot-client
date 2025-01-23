d84eea93ccaa0e5aaa431a7a47703c97e8dc85a6
Mungjin01 <beckaljm@naver.com>
2025-01-23 21:27:36 +0900

    chore: vite.main 수정 (#10)

--- apps/electron/vite.main.config.ts
+++ apps/electron/vite.main.config.ts
@@ -3,14 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     outDir: 'dist',
-    target: 'node14',
     rollupOptions: {
       external: ['path', 'url', 'fs', 'electron'],
-    },
-  },
-  resolve: {
-    alias: {
-      path: 'path-browserify',
     },
   },
 });
