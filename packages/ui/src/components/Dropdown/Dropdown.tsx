59cd249d4461d37fda007098f42b3a803e3de533
Mungjin01 <beckaljm@naver.com>
2025-02-12 12:29:06 +0900

    feat: 드롭다운 item 취소 기능 추가 (#9)

--- packages/ui/src/components/Dropdown/Dropdown.tsx
+++ packages/ui/src/components/Dropdown/Dropdown.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import styled from '@emotion/styled';
 import { Icon } from '../Icon/Icon';
+import { useTheme } from '@emotion/react';
 
 interface DropdownProps {
   title: string;
@@ -17,59 +18,67 @@ export const Dropdown = ({
   setSelectedContents,
   width = '312px',
 }: DropdownProps) => {
-  const [open, setOpen] = useState<boolean>(false);
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
   const selectContainerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const oustideClickHandler = (e: MouseEvent) => {
-      if (
-        selectContainerRef.current &&
-        !selectContainerRef.current.contains(e.target as HTMLElement)
-      ) {
-        setOpen(false);
-      }
-    };
-    document.addEventListener('click', oustideClickHandler);
+  const oustideClickHandler = (e: MouseEvent) => {
+    if (
+      selectContainerRef.current &&
+      !selectContainerRef.current.contains(e.target as HTMLElement)
+    ) {
+      setOpen(false);
+    }
+  };
 
-    return () => {
-      document.removeEventListener('click', oustideClickHandler);
-    };
+  useEffect(() => {
+    document.addEventListener('click', oustideClickHandler);
+    return () => document.removeEventListener('click', oustideClickHandler);
   }, []);
+
+  const handleToggleDropdown = () => setOpen((prev) => !prev);
+
+  const handleSelectContent = (content: string) => {
+    setSelectedContents([
+      ...(selectedContents.includes(content)
+        ? selectedContents.filter((item) => item !== content)
+        : [...selectedContents, content]),
+    ]);
+  };
 
   return (
     <Dropdowns ref={selectContainerRef} width={width}>
-      <DropdownContent onClick={() => setOpen((prev) => !prev)}>
-        <DropdownLabel>
-          <span>{title}</span>
-        </DropdownLabel>
+      <DropdownHeader onClick={handleToggleDropdown}>
+        <DropdownLabel>{title}</DropdownLabel>
         <DropdownSelected>
           {selectedContents.length === 0 ? (
             <span className="no-filter"> </span>
           ) : (
             <span className="selected">
-              {`${selectedContents[0]}${selectedContents.length > 1 ? ` 외 ${selectedContents.length - 1}` : ''}`}
+              {`${selectedContents[0]}${
+                selectedContents.length > 1
+                  ? ` 외 ${selectedContents.length - 1}`
+                  : ''
+              }`}
             </span>
           )}
-          {open ? (
-            <Icon name="arrowDown" size={20} />
-          ) : (
-            <Icon name="arrowUp" size={20} />
-          )}
+          <Icon
+            name={open ? 'arrowDown' : 'arrowUp'}
+            fill={theme.palette.yellow500}
+            size={24}
+          />
         </DropdownSelected>
-      </DropdownContent>
+      </DropdownHeader>
       {open && (
         <DropdownExpandContent>
           {contents.map((content) => (
-            <div
+            <DropdownItem
               key={content}
-              className="content"
-              onClick={() => {
-                if (!selectedContents.includes(content))
-                  setSelectedContents([...selectedContents, content]);
-              }}
+              onClick={() => handleSelectContent(content)}
+              selected={selectedContents.includes(content)}
             >
               {content}
-            </div>
+            </DropdownItem>
           ))}
         </DropdownExpandContent>
       )}
@@ -85,37 +94,28 @@ const Dropdowns = styled.div<{ width: string }>`
   width: ${({ width }) => width};
 `;
 
-const DropdownContent = styled.div`
+const DropdownHeader = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-
   gap: 20px;
   padding: 0 16px;
   height: 48px;
   border-radius: 8px;
   border: 1px solid ${({ theme }) => theme.palette.gray200};
-
   background: ${({ theme }) => theme.palette.white};
   cursor: pointer;
 `;
 
-const DropdownLabel = styled.div`
-  display: flex;
-  gap: 10px;
-
+const DropdownLabel = styled.span`
   ${({ theme }) => theme.typo.body1m};
-
-  .filter-title {
-    color: ${({ theme }) => theme.palette.gray700};
-  }
+  color: ${({ theme }) => theme.palette.gray700};
 `;
 
 const DropdownSelected = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
-
   ${({ theme }) => theme.typo.body1m};
 
   .no-filter {
@@ -133,24 +133,25 @@ const DropdownExpandContent = styled.div`
   z-index: 2;
   display: flex;
   flex-direction: column;
-
   background: ${({ theme }) => theme.palette.white};
   border: 1px solid ${({ theme }) => theme.palette.gray200};
-
-  overflow: hidden;
   width: 100%;
   border-radius: 8px;
+  overflow: hidden;
+`;
 
-  .content {
-    padding: 12px 24px;
-    height: 48px;
-    align-items: center;
-    ${({ theme }) => theme.typo.body1m};
-    color: ${({ theme }) => theme.palette.gray800};
-    background-color: ${({ theme }) => theme.palette.white};
+const DropdownItem = styled.button<{ selected: boolean }>`
+  padding: 12px 24px;
+  height: 48px;
+  text-align: left;
+  ${({ theme }) => theme.typo.body1m};
+  color: ${({ selected, theme }) =>
+    selected ? theme.palette.yellow500 : theme.palette.gray800};
+  background-color: ${({ theme }) => theme.palette.white};
+  border: none;
+  cursor: pointer;
 
-    &:hover {
-      background-color: ${({ theme }) => theme.palette.gray100};
-    }
+  &:hover {
+    background-color: ${({ theme }) => theme.palette.gray100};
   }
 `;
