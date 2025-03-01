1ee970abeb8d5f7d84e1ba3bcd2cd56801be9df5
Mungjin01 <beckaljm@naver.com>
2025-03-01 15:14:54 +0900

    chore: node-abi, electron-rebuild 설치

--- apps/electron/src/main.ts
+++ apps/electron/src/main.ts
@@ -1,8 +1,11 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'node:path';
 
+const nativeModule = require(
+  path.join(__dirname, '../../../native/build/Release/module.node')
+);
+
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -12,6 +15,8 @@ const createWindow = () => {
     },
   });
 
+  console.log(nativeModule);
+
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
@@ -20,19 +25,18 @@ const createWindow = () => {
       path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
     );
   }
-
-  // Open the DevTools.
+  /* 
+  mainWindow.loadURL('http://localhost:5173');
+  logMessage('렌더러 로드 );*/
   mainWindow.webContents.openDevTools();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  process.env.LANG = 'en_US.UTF-8';
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+  createWindow();
+});
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -40,12 +44,7 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
