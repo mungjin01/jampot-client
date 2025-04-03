de728c51d699b24b41e414209b7972753f20a4ef
Mungjin01 <beckaljm@naver.com>
2025-04-03 11:39:35 +0900

    chore: 일렉트론 webrtc 테스트 수정(#30)

--- apps/electron/vite.renderer.config.ts
+++ apps/electron/vite.renderer.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import stdLibBrowser from 'vite-plugin-node-stdlib-browser';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import * as path from 'path';
@@ -6,26 +7,43 @@ import * as fs from 'fs';
 
 export default defineConfig({
   plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-    }),
+    react({ jsxImportSource: '@emotion/react' }),
     svgr(),
+    stdLibBrowser(),
   ],
+  resolve: {
+    alias: {
+      '@repo/ui': path.resolve(__dirname, '../../packages/ui/src'),
+      events: 'rollup-plugin-node-polyfills/polyfills/events',
+      util: 'rollup-plugin-node-polyfills/polyfills/util',
+    },
+  },
+  optimizeDeps: {
+    include: [
+      'debug',
+      'ms',
+      'ua-parser-js',
+      'sdp-transform',
+      'awaitqueue',
+      'queue-microtask',
+      'h264-profile-level-id',
+      'bowser',
+      'events',
+      'util',
+    ],
+  },
   build: {
     rollupOptions: {
-      external: [],
+      external: ['npm-events-package'],
+    },
+    commonjsOptions: {
+      include: [/node_modules/],
     },
   },
   server: {
     https: {
       key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
       cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
-    },
-  },
-
-  resolve: {
-    alias: {
-      '@repo/ui': path.resolve(__dirname, '../../packages/ui/src'),
     },
   },
 });
